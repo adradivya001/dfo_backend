33c175787d0a503f3f19ab6cbf6e77869dff595c
adradivya001 <adradivya001@github.com>
2026-04-07 09:48:55 +0530

    feat: synchronize clinical OS backend (Clinical OS Sync)

--- control-tower-core/src/domains/janmasethu/channel/messaging.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module, Global } from '@nestjs/common';
-import { JanmasethuDispatchService } from './janmasethu-dispatch.service';
-
-@Global()
-@Module({
-    providers: [JanmasethuDispatchService],
-    exports: [JanmasethuDispatchService],
-})
-export class MessagingModule { }
